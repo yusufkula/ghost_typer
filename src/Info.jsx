4a6b2f77d830b25379ab77eded4908fa2b468f11
Yusuf Kula <yvk2045@gmail.com>
2024-03-09 16:38:53 +0000

    add info component

---
+++ src/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Info = () => {
+    return (
+        <div className="infoBox">
+            <p className="incorrect">Wrong</p>
+            <p className="activeWord">Active</p>
+            <p className="correct">Correct</p>
+        </div>
+    )
+}
+
+export default Info
